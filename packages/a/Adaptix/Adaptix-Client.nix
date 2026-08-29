aa7b27a7257843626433b5d014d6b8ecdf66755c
terminalrally <50644033+terminalrally@users.noreply.github.com>
2026-08-29 19:28:16 -0400

    feat: add Adaptix pkg

---
+++ packages/a/Adaptix/Adaptix-Client.nix
@@ -0,0 +1,49 @@
+{ stdenv, lib, pkgs, fetchFromGitHub, cmake, qt6, ... }:
+
+stdenv.mkDerivation rec {
+  name = "adaptix-client";
+  version = "31967d8";
+
+  src = fetchFromGitHub {
+    owner = "Adaptix-Framework";
+    repo = "AdaptixC2";
+    rev = "31967d85dcc181451659fb60a8861d70e61f6a08";
+    sha256 = "sha256-YwNFDmStLyU3GLCNVcLk646Pc1gLIpIAsRPckQNyqVc=";
+  };
+
+  nativeBuildInputs = with pkgs; [
+    qt6.wrapQtAppsHook
+    gnumake
+    cmake
+  ];
+
+  buildInputs = with qt6; [
+    qtbase
+    qtdeclarative
+    qtwebengine
+  ];
+
+  # Disable automatic cmake configuration - the Makefile runs cmake itself
+  dontUseCmakeConfigure = true;
+
+  buildPhase = ''
+    runHook preBuild
+    make client
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp dist/AdaptixClient $out/bin/
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description =
+      "AdaptixC2 is a Command and Control framework designed for red team operations and adversary simulations.";
+    homepage = "";
+    license = licenses.free;
+    maintainers = [ ];
+  };
+}
---
+++ packages/a/Adaptix/Adaptix-Server.nix
@@ -0,0 +1,41 @@
+{ lib, pkgs, buildGoModule, fetchFromGitHub, go_1_25, ... }:
+
+(buildGoModule.override { go = go_1_25; }) rec {
+  name = "adaptix-server";
+  version = "31967d8";
+
+  src = fetchFromGitHub {
+    owner = "Adaptix-Framework";
+    repo = "AdaptixC2";
+    rev = "31967d85dcc181451659fb60a8861d70e61f6a08";
+    sha256 = "sha256-YwNFDmStLyU3GLCNVcLk646Pc1gLIpIAsRPckQNyqVc=";
+  };
+
+  sourceRoot = "source/AdaptixServer";
+
+  vendorHash = "sha256-cONOdVNkYC0bv8wowuG0SPovf24un/WvhBI47H7rpFI=";
+  proxyVendor = true;
+
+  env.GOEXPERIMENT = "jsonv2,greenteagc";
+
+  ldflags = [
+    "-s" # strip symbols
+    "-w" # strip DWARF
+  ];
+
+  nativeBuildInputs = with pkgs; [ gnumake ];
+
+  subPackages = [ "." ];
+
+  postInstall = ''
+    cp ssl_gen.sh profile.json 404page.html $out/bin/
+  '';
+
+  meta = with lib; {
+    description =
+      "AdaptixC2 is a Command and Control framework designed for red team operations and adversary simulations.";
+    homepage = "";
+    license = licenses.free;
+    maintainers = [ ];
+  };
+}
